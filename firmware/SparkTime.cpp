146c53c5add283b2d474abff44653d465909588f
Brian Ogilvie <bko@pobox.com>
2015-07-04 11:57:00 -0400

    Update SparkTime.cpp

--- firmware/SparkTime.cpp
+++ firmware/SparkTime.cpp
@@ -22,8 +22,7 @@
      CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
      ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
      POSSIBILITY OF SUCH DAMAGE.
-
-   TODO: Fractional second handling for NTP and millis();
+   TODO:
          Translation for other languages
 
 */
